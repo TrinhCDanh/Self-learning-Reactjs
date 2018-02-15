bf618649e2bd9698821b71dca219e58569b05883
TrinhCDanh <trinhdanh04@gmail.com>
2018-02-15 19:40:10 +0700

    Event

--- ch01/src/components/Course.js
+++ ch01/src/components/Course.js
@@ -2,10 +2,33 @@ import React, { Component } from 'react';
 import Lesson from './Lesson'
 
 class Course extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.handleClick3 = this.handleClick3.bind(this);
+	}
+
+	handleClick1() {
+		alert(123);
+	}
+
+	handleClick2(text = "Xmen") {
+		alert(text);
+	}
+
+	handleClick3() {
+		alert(this.props.name);
+	}
+
 	checkFree() {
 		let free = this.props.free;
 		if (free)
-			return <a className="btn btn-primary" href="#" role="button">View</a>
+			return 	<div className="btn-group"> 
+								<a onClick={this.handleClick1} className="btn btn-primary" href="#" role="button">View 1</a>
+								<a onClick={() => this.handleClick2("Hello World!")} className="btn btn-danger" href="#" role="button">View 2</a>
+								<a onClick={this.handleClick3} className="btn btn-warning" href="#" role="button">View 3</a>
+							</div>
 	}
   render() {
     return (
