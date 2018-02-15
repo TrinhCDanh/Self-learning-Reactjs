52c2f6e75f57cf612fca26eb443349d0935ca732
TrinhCDanh <trinhdanh04@gmail.com>
2018-02-15 12:20:31 +0700

    JSX Forech

--- ch01/src/App.js
+++ ch01/src/App.js
@@ -5,11 +5,29 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const ArrCourse = [
+      {
+        name: "React",
+        free: true,
+        desc: "React is very simple"
+      },
+      {
+        name: "Nodejs",
+        free: true
+      },
+      {
+        name: "Laravel",
+        free: false
+      }
+    ];
+
+    let ItemCourse = ArrCourse.map((elem, index) => 
+      <Course key={index} name={elem.name} free={elem.free}>{elem.desc}</Course>
+    );
+
     return (
       <div className="row equal">
-        <Course name="React" free={true}>React is very simple</Course>
-        <Course name="Nodejs" free={true}/>
-        <Course name="Laravel" free={false}/>
+        {ItemCourse}
       </div>
     );
   }
